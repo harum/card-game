056bf2cc21f74596fb461430fc206f610c8ad751
Putu Harum Bawa <lightharum.108@gmail.com>
2018-03-11 21:39:57 +0700

    add socket.io

--- server.js
+++ server.js
@@ -4,12 +4,22 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 app.get('/ping', function (req, res) {
  return res.send('pong');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+http.listen(process.env.PORT || 8080);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import io from 'socket.io-client';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+const socket = io('http://localhost:8080');
+socket.on('connect', () => { console.log('connected')});
+socket.on('event', (data) => { console.log('data', data)});
+socket.on('disconnect', () => { console.log('disconnected')});
+
