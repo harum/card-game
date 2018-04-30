62af3824823d208422a470646eaace5b01dc8f6f
rezamauliadi <reza.mauliadi2@gmail.com>
2018-04-30 17:16:08 +0700

    add card story

--- src/components/Card/Card.story.js
+++ src/components/Card/Card.story.js
@@ -7,6 +7,39 @@ import Card from './index';
 
 // Story Config
 const story = Lib.storiesOf('Card', module);
+story.addDecorator(Lib.withKnobs);
 
-// Default
-story.add('Default', () => (<Card />));
+const numOptions = {
+  1: '1',
+  2: '2',
+  3: '3',
+  4: '4',
+  5: '5',
+  6: '6',
+  7: '7',
+  8: '4',
+  9: '5',
+  J: 'J',
+  Q: 'Q',
+  K: 'K',
+  A: 'A'
+};
+
+const typeOptions = {
+  spade: 'spade',
+  heart: 'heart',
+  club: 'club',
+  diamond: 'diamond'
+};
+
+story.add('Default Card', () => {
+  const num = Lib.select('number', numOptions, 'A');
+  const type = Lib.select('type', typeOptions, 'spade');
+
+  return (
+    <Card
+      type={type}
+      number={num}
+    />
+  );
+});
