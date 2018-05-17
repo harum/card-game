a98daf0fe4c94c4bc2065cb4fc3e3e865eaeec3d
rezamauliadi <reza.mauliadi2@gmail.com>
2018-05-17 14:01:24 +0700

    hand cards width from js

--- src/components/HandCards/index.js
+++ src/components/HandCards/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '../Card';
+import cardDim from '../../helpers/cardDimension';
 import './style.scss';
 
-const CARD_SPACE = 30;
+const getHandCardStyle = numberOfCards => ({
+  width: `${(cardDim.space * (numberOfCards - 1)) + cardDim.oneHalfWidth}px`
+});
 
-const getCardStyle = (card, index) =>
-  ({
-    zIndex: `${index}`,
-    bottom: `${(card.picked ? CARD_SPACE : 0)}px`
-  });
+const getCardStyle = (card, index) => ({
+  zIndex: `${index}`,
+  bottom: `${(card.picked ? cardDim.space : 0)}px`
+});
 
 const HandCards = ({ cards, onCardClicked }) => {
   const pickCard = index => onCardClicked(index);
 
   return (
-    <div className="c-hand-cards">
+    <div
+      className="c-hand-cards"
+      style={getHandCardStyle(cards.length)}
+    >
       { cards.map((card, index) =>
         (
           <div
