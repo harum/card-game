975dc3ade57524a9632bdd647399859cffa142c3
rezamauliadi <reza.mauliadi@bukalapak.com>
2018-05-18 15:53:27 +0700

    Table and Deck (#2)
    * refactor table using grid css
    
    * fix style left and right card placeholder
    
    * add card zoom variable
    
    * make some hardcoded values to variables
    
    * add card width one half and height one half var
    
    * hand cards width from js
    
    * fix cc
    
    * fix last cc
    
    * change card placeholder name

---
+++ src/helpers/cardDimension.js
@@ -0,0 +1,13 @@
+// same as setting global style
+const CARD_SPACE = 30;
+const CARD_BASE_WIDTH = 64;
+const CARD_BASE_HEIGHT = 89;
+const CARD_ZOOM = 1.5;
+
+export default {
+  space: CARD_SPACE,
+  baseWidth: CARD_BASE_WIDTH,
+  baseHeight: CARD_BASE_HEIGHT,
+  oneHalfWidth: CARD_BASE_WIDTH * CARD_ZOOM,
+  oneHalfHeight: CARD_BASE_HEIGHT * CARD_ZOOM
+};
