a8fab7a9561d0b43be1d992a6ce265f7b084cf0b
faizkautsarr <faizkautsarr@gmail.com>
2018-09-20 19:07:33 +0700

    fix cc

--- src/helpers/firstTurnFlag/index.test.js
+++ src/helpers/firstTurnFlag/index.test.js
@@ -3,22 +3,18 @@ import checkFirstTurn from './index';
 import shuffleDeck from '../../helpers/shuffleDeck';
 
 const cardsFullRandomized = shuffleDeck(fullDeck);
-const player1 = {
-  cards: cardsFullRandomized.slice(0, 13),
-  isFirstTurn: checkFirstTurn(cardsFullRandomized.slice(0, 13))
-};
-const player2 = {
-  cards: cardsFullRandomized.slice(13, 26),
-  isFirstTurn: checkFirstTurn(cardsFullRandomized.slice(13, 26))
-};
-const player3 = {
-  cards: cardsFullRandomized.slice(26, 39),
-  isFirstTurn: checkFirstTurn(cardsFullRandomized.slice(26, 39))
-};
-const player4 = {
-  cards: cardsFullRandomized.slice(39, 52),
-  isFirstTurn: checkFirstTurn(cardsFullRandomized.slice(39, 52))
-};
+
+const insertPlayerAndObject = (a, b) => (
+  {
+    cards: cardsFullRandomized.slice(a, b),
+    isFirstTurn: checkFirstTurn(cardsFullRandomized.slice(a, b))
+  }
+);
+
+const player1 = insertPlayerAndObject(0, 13);
+const player2 = insertPlayerAndObject(13, 26);
+const player3 = insertPlayerAndObject(26, 39);
+const player4 = insertPlayerAndObject(39, 52);
 
 const firstTurnPlayer = (a, b, c, d) => {
   if (a.isFirstTurn) {
