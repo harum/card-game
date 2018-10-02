7d5f40f02c3a79af2824b162d3f93f30d76ed31a
rezamauliadi <reza.mauliadi2@gmail.com>
2018-10-02 14:48:50 +0700

    refactor popupcontent and enemy card to fix cc

--- src/components/ConfirmationPopup/PopupContent.js
+++ src/components/ConfirmationPopup/PopupContent.js
@@ -7,30 +7,32 @@ const PopupContent = ({
   cancelCard, eligible, putCard, foldCard, hasEligibleCard
 }) => {
   const content = () => {
-    if (hasEligibleCard && eligible) {
-      return (
-        <div>
-          <h4>Put this card in the arena?</h4>
-          <Button text="Cancel" onClick={cancelCard} />
-          <Button text="Put" onClick={putCard} color="blue" />
-        </div>
-      );
-    } else if (hasEligibleCard && !eligible) {
+    if (hasEligibleCard) {
+      if (eligible) {
+        return (
+          <div>
+            <h4>Put this card in the arena?</h4>
+            <Button text="Cancel" onClick={cancelCard} />
+            <Button text="Put" onClick={putCard} color="blue" />
+          </div>
+        );
+      }
+
       return (
         <div>
           <h4>You still have eligible card!</h4>
           <Button text="OK" onClick={cancelCard} />
         </div>
       );
-    } else if (!hasEligibleCard && !eligible) {
-      return (
-        <div>
-          <h4>Fold this card?</h4>
-          <Button text="Fold" onClick={foldCard} color="red" />
-        </div>
-      );
     }
-    return '';
+
+    return (
+      <div>
+        <h4>Fold this card?</h4>
+        <Button text="Cancel" onClick={cancelCard} />
+        <Button text="Fold" onClick={foldCard} color="red" />
+      </div>
+    );
   };
 
   return content();
